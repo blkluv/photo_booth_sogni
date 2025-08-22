1f708d1634476814c3928436ffd8fab07da46d4b
Mark Ledford <markledford@gmail.com>
2025-08-21 21:31:34 -0700

    mobile app PWA and css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import './styles/style-dropdown.css';
 import './styles/ios-chrome-fixes.css';
 import './styles/mobile-portrait-fixes.css'; // New critical mobile portrait fixes
 import './styles/mobile-chrome-fixes.css'; // Chrome mobile context menu fixes
+import './styles/pwa-standalone-fixes.css'; // PWA standalone mode fixes
 import CameraView from './components/camera/CameraView';
 import CameraStartMenu from './components/camera/CameraStartMenu';
 import StyleDropdown from './components/shared/StyleDropdown';
